9101acfebae750fa4e71fdb777fa76b7e39aa598
Lars <lbersch@gwdg.de>
2015-11-20 16:35:51 +0100

    code cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -39,90 +39,89 @@ using namespace openni;
 int main(int argc, char *argv[])
 {
 	// Initialize OpenNI
-    if (OpenNI::initialize() != STATUS_OK)
-  	{
+    if (STATUS_OK != OpenNI::initialize())
+	{
         std::cerr << "OpenNI Initial Error: "  << OpenNI::getExtendedError() << std::endl;
     	return -1;
-  	}
+	}
   
-  	// Open Device
+	// Open Device
     Device device;
-    if (device.open( ANY_DEVICE ) != STATUS_OK)
-  	{
+    if (STATUS_OK != device.open( ANY_DEVICE ))
+	{
         std::cerr << "Can't Open Device: "  << OpenNI::getExtendedError() << std::endl;
     	return -1;
-  	}
+	}
   
-  	// Create depth stream
-  	VideoStream depth_stream;
-    if (depth_stream.create( device, SENSOR_DEPTH ) == STATUS_OK)
+	// Create depth stream
+	VideoStream depth_stream;
+    if (STATUS_OK == depth_stream.create( device, SENSOR_DEPTH ))
     {
-      	// 3a. set video mode
-      	VideoMode mode;
-      	mode.setResolution(WIDTH, HEIGHT);
-      	mode.setFps(30);
-     	mode.setPixelFormat(PIXEL_FORMAT_DEPTH_1_MM);
-  
-      	if ( depth_stream.setVideoMode(mode) != STATUS_OK )
-       		std::cout << "Can't apply VideoMode: " << OpenNI::getExtendedError() << std::endl;
+	   // 3a. set video mode
+    	VideoMode mode;
+    	mode.setResolution(WIDTH, HEIGHT);
+    	mode.setFps(30);
+    	mode.setPixelFormat(PIXEL_FORMAT_DEPTH_1_MM);
+
+    	if ( depth_stream.setVideoMode(mode) != STATUS_OK )
+    		std::cout << "Can't apply VideoMode: " << OpenNI::getExtendedError() << std::endl;
     }
     else
     {
-      	std::cerr << "Can't create depth stream on device: " << OpenNI::getExtendedError() << std::endl;
-      	return -1;
+    	std::cerr << "Can't create depth stream on device: " << OpenNI::getExtendedError() << std::endl;
+    	return -1;
     }
   
-  	// Create color stream
-  	VideoStream color_stream;
-  	if (device.hasSensor(SENSOR_COLOR))
-  	{
-    	if (color_stream.create(device, SENSOR_COLOR) == STATUS_OK)
+	// Create color stream
+	VideoStream color_stream;
+	if (device.hasSensor(SENSOR_COLOR))
+	{
+    	if (STATUS_OK == color_stream.create(device, SENSOR_COLOR))
     	{
-      		// 4a. set video mode
-      		VideoMode mode;
-      		mode.setResolution(WIDTH, HEIGHT);
-      		mode.setFps(30);
-      		mode.setPixelFormat(PIXEL_FORMAT_RGB888);
-  
-      		if (color_stream.setVideoMode(mode) != STATUS_OK)
+    		// 4a. set video mode
+		    VideoMode mode;
+    		mode.setResolution(WIDTH, HEIGHT);
+    		mode.setFps(30);
+    		mode.setPixelFormat(PIXEL_FORMAT_RGB888);
+            
+    		if (color_stream.setVideoMode(mode) != STATUS_OK)
         		std::cout << "Can't apply VideoMode: " << OpenNI::getExtendedError() << std::endl;
-  
-      		// 4b. image registration
-      		if (device.isImageRegistrationModeSupported(IMAGE_REGISTRATION_DEPTH_TO_COLOR))
+                
+    		// 4b. image registration
+    		if (device.isImageRegistrationModeSupported(IMAGE_REGISTRATION_DEPTH_TO_COLOR))
         		device.setImageRegistrationMode(IMAGE_REGISTRATION_DEPTH_TO_COLOR);
     	}
     	else
     	{
-      		std::cerr << "Can't create color stream on device: " << OpenNI::getExtendedError() << std::endl;
-      		return -1;
+    		std::cerr << "Can't create color stream on device: " << OpenNI::getExtendedError() << std::endl;
+    		return -1;
     	}
-  	}
-  	
-  	
+	}
+	
+	
     // Print help
-  	std::cout << "c - calibrate pose and time \n"
+	std::cout << "c - calibrate pose and time \n"
         << "p - subtract background \n"
         << "t - save tracking data \n"
         << "s - save frame \n"
         << "r / b / g / y - red / blue / green / yellow color analysis \n"
         << "q - quit" << std::endl;
-  	
+	
 
     // Init color and depth frames, start streames
-  	VideoFrameRef color_frame;
-  	VideoFrameRef depth_frame;
-  	depth_stream.start();
-  	color_stream.start();
-  	int max_depth = depth_stream.getMaxPixelValue();
-  	
+	VideoFrameRef color_frame;
+	VideoFrameRef depth_frame;
+	depth_stream.start();
+	color_stream.start();
+	int max_depth = depth_stream.getMaxPixelValue();	
     
     // Read first frame
-    if (color_stream.readFrame( &color_frame ) != STATUS_OK)
+    if (STATUS_OK != color_stream.readFrame(&color_frame))
     {
         std::cerr << "Can't read color frame'" << std::endl;
         return -1;
     }
-    if (depth_stream.readFrame( &depth_frame ) != STATUS_OK)
+    if (STATUS_OK != depth_stream.readFrame(&depth_frame))
     {
         std::cerr << "Can't read depth frame'" << std::endl;
         return -1;
@@ -163,8 +162,8 @@ int main(int argc, char *argv[])
     std::vector<Pose> pose_history;
     int current_time = getTime();
     std::vector<Sphere> current_spheres;
-  	while (true)
-  	{
+	while (true)
+	{
     	// Check possible errors
     	if (!color_stream.isValid())
     	{
@@ -172,27 +171,27 @@ int main(int argc, char *argv[])
     		break;
     	}
     	
-      	if (color_stream.readFrame( &color_frame ) != STATUS_OK)
-      	{
-      		std::cerr << "Can't read color frame'" << std::endl;
-    		break;
-      	}
-      	
-      	if (depth_stream.readFrame( &depth_frame ) != STATUS_OK)
+    	if (color_stream.readFrame( &color_frame ) != STATUS_OK)
     	{
-      		std::cerr << "Can't read depth frame'" << std::endl;
+    		std::cerr << "Can't read color frame'" << std::endl;
     		break;
     	}
-      	
+    	
+    	if (depth_stream.readFrame( &depth_frame ) != STATUS_OK)
+    	{
+    		std::cerr << "Can't read depth frame'" << std::endl;
+    		break;
+    	}
+    	
         
-      	// Grab frames, result is frame and frame_depth
-      	const cv::Mat color_frame_temp( color_frame.getHeight(), color_frame.getWidth(), CV_8UC3, (void*)color_frame.getData() );
+    	// Grab frames, result is frame and frame_depth
+    	const cv::Mat color_frame_temp( color_frame.getHeight(), color_frame.getWidth(), CV_8UC3, (void*)color_frame.getData() );
         cv::Mat frame;
         cv::cvtColor(color_frame_temp, frame, CV_RGB2BGR);
         
     	const cv::Mat depth_frame_temp( depth_frame.getHeight(), depth_frame.getWidth(), CV_16UC1, (void*)depth_frame.getData() );
-      	cv::Mat frame_depth;
-      	depth_frame_temp.convertTo(frame_depth, CV_8U, 255.0 / max_depth);
+    	cv::Mat frame_depth;
+    	depth_frame_temp.convertTo(frame_depth, CV_8U, 255.0 / max_depth);
         
         
         // Get current time and time difference
@@ -202,8 +201,8 @@ int main(int argc, char *argv[])
         
 
         // Detect spheres
-      	cv::Mat frame_info = frame.clone();
-      	std::vector<Measurement> measurements = sphere_detector.findSpheres(frame_info, frame_depth, current_time);
+    	cv::Mat frame_info = frame.clone();
+    	std::vector<Measurement> measurements = sphere_detector.findSpheres(frame_info, frame_depth, current_time);
         
         
         // Update filter and get tracked spheres
@@ -264,7 +263,7 @@ int main(int argc, char *argv[])
                 return 0;
                 break;
         }
-  	}
+	}
     
     // Destroy objects, otherwise errors will occure next time...
 	depth_stream.destroy();
