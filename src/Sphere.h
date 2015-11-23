f401a26718ee48db61e318ba816ad02be1986afa
Lars <lbersch@gwdg.de>
2015-11-23 14:05:18 +0100

    code cleaning

---
+++ src/Sphere.h
@@ -0,0 +1,18 @@
+#ifndef _SPHERE_H_
+#define _SPHERE_H_
+
+#include "Helper.h"
+
+
+struct Sphere
+{
+    Color color;
+    Eigen::Vector3f position;
+	
+	Sphere(Eigen::Vector3f position, Color color) : position(position), color(color) {};
+    Sphere(float x, float y, float z, Color color) : color(color) {
+        position << x, y, z;
+    };
+};
+
+#endif /* _SPHERE_H_ */
